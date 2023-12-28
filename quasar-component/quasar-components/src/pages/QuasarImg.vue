6ab73c04971faffa9ef794bae231692ca46e259f
AKSHAY SHIRSAT <arsofficial28@gmail.com>
2023-12-28 18:16:10 +0530

    comit

--- quasar-component/quasar-components/src/pages/QuasarImg.vue
+++ quasar-component/quasar-components/src/pages/QuasarImg.vue
@@ -15,7 +15,33 @@
         <q-icon  name="fa-solid fa-house" size="2em"></q-icon>
         </div>
       </div>
+      <div class="q-pa-md q-gutter-sm">
+    <q-btn push color="teal" label="Change image" @click="refresh" />
 
+    <q-img
+    :src="url"
+      spinner-color="white"
+      style="height: 140px; max-width: 300px"
+    />
+  </div>
     </div>
   </q-page>
 </template>
+
+<script>
+
+export default{
+data(){
+  return{
+    url:'https://picsum.photos/500/300'
+  }
+},
+
+methods:{
+refresh(){
+ this.url = 'https://picsum.photos/500/300?t=' + Math.random()
+}
+}
+}
+
+</script>
